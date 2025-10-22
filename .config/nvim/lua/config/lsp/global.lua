dae5991c23b04575c8210d7e21a85846ff045ea3
Riccardo Padoan <padoanr@fastmail.com>
2025-10-22 15:50:38 +0100

    nvim: switch to sidekick over codecompanion

--- .config/nvim/lua/config/lsp/global.lua
+++ .config/nvim/lua/config/lsp/global.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
             vim.lsp.buf.definition()
         end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>ac", function() vim.lsp.buf.code_action() end, opts)
+        -- vim.keymap.set("n", "<leader>ac", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>fr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("n", "<leader>fm", function() vim.lsp.buf.format() end, opts)
@@ -27,11 +27,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
         end, opts)
 
         -- format on save
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function()
-                vim.lsp.buf.format({ async = true })
-            end,
-        })
+        -- vim.api.nvim_create_autocmd("BufWritePre", {
+        --     callback = function()
+        --         vim.lsp.buf.format({ async = true })
+        --     end,
+        -- })
     end,
 })
 
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,5 +19,14 @@ return {
                 "bashls",
             }
         })
+        -- copilot / sidekick inline completion
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+                local client = vim.lsp.get_client_by_id(args.data.client_id)
+                if client and client:supports_method("textDocument/inlineCompletion") then
+                    vim.lsp.inline_completion.enable(true)
+                end
+            end,
+        })
     end
 }
